83942a11ca75eef197e7795a2a5c5b14740bd866
Andrew Xia <axia@mit.edu>
2017-06-27 11:46:27 -0400

    reference code for sys, json, pickle

---
+++ reference-code/sys-argv.py
@@ -0,0 +1,9 @@
+# June 27 2017
+# Andrew Xia
+# using system arguments in python 
+# http://www.pythonforbeginners.com/system/python-sys-argv
+
+import sys
+print "This is the name of the script: ", sys.argv[0]
+print "Number of arguments: ", len(sys.argv)
+print "The arguments are: " , str(sys.argv)
